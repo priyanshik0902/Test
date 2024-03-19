96b4f97991aed6f5b57b3c6477377764ff23af56
priyanshik0902 <latakotian09@gmail.com>
2024-03-20 01:16:20 +0530

    Initial commit

---
+++ src/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className="h-1/2 scroll-snap-align-center text-white">
+    Contact ojojojoj
+    </div>
+  )
+}
+
+export default LandingPage
